c9c92e0279aef1e455384808c9571cd546ea5e64
César Costa <cesarrcosta99@gmail.com>
2024-04-27 10:04:43 -0300

    Projeto final

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,23 +1,37 @@
 import { useState } from "react";
-import PropTypes from  'prop-types'
+import PropTypes from "prop-types";
 import Icon from "../../assets/Icon.png";
-import {SecondContainer, RingsMovies, Finter } from "./styles";
+import { SecondContainer, RingsMovies, Finter } from "./styles";
 import { useEffect } from "react";
 
-function Header({docs,setFilterDocs}) {
-  const [nameInput,setNameInput]=useState("")
+function Header({ docs, setFilterDocs }) {
+  const [nameInput, setNameInput] = useState("");
+  const [runTime, setRunTime] = useState({
+    time: null,
+    budget: null,
+  });
 
-  useEffect(()=>{
-    const filterDocs=docs.filter(doc=>doc.name.toUpperCase().includes(nameInput.toUpperCase()))
-    setFilterDocs(filterDocs)
-  },[docs, nameInput, setFilterDocs])
+  useEffect(() => {
+    const filterDocs = docs.filter((doc) =>
+      doc.name.toUpperCase().includes(nameInput.toUpperCase())
+    );
+    setFilterDocs(filterDocs);
 
+    const resultTime = filterDocs.reduce((acc, valorAtual) => {
+      return acc + valorAtual.runtimeInMinutes;
+    }, 0);
 
-  function handleChange(e){
-    setNameInput(e.target.value)
+     filterDocs.length > 0 && setRunTime({
+      time: resultTime / filterDocs.length,
+      budget: filterDocs.reduce((acc, valorAtual) => {
+        return acc + valorAtual.budgetInMillions;
+      }, 0),
+    });
+  }, [docs, nameInput, setFilterDocs]);
+
+  function handleChange(e) {
+    setNameInput(e.target.value);
   }
-
-
 
   return (
     <>
@@ -28,17 +42,21 @@ function Header({docs,setFilterDocs}) {
       <RingsMovies>
         <Finter>
           <h1>Lord of the Rings Movies</h1>
-          <p>Ave. movie runtime: xxx min</p>
-          <p>Ave. movie budget: $XXM</p>
+          <p>Ave. movie runtime:{runTime.time} min</p>
+          <p>Ave. movie budget: ${runTime.budget}M</p>
         </Finter>
-        <input onChange={handleChange} type="text" placeholder="Finter movies by name" />
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="Finter movies by name"
+        />
       </RingsMovies>
     </>
   );
 }
-Header.propTypes={
-  docs:PropTypes.array,
-  setFilterDocs:PropTypes.func
-}
+Header.propTypes = {
+  docs: PropTypes.array,
+  setFilterDocs: PropTypes.func,
+};
 
 export default Header;
